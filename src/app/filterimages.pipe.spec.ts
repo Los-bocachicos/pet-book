47132661b3c19774b42116a2e9ca9801477afc71
Mateo Arboleda <mateo.arbr@gmail.com>
2020-08-30 12:06:05 -0500

    Add some test to gallery and filter

--- src/app/filterimages.pipe.spec.ts
+++ src/app/filterimages.pipe.spec.ts
@@ -5,4 +5,27 @@ describe('FilterimagesPipe', () => {
     const pipe = new FilterimagesPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should filter all', () => {
+    const pipe = new FilterimagesPipe();
+    expect(pipe.transform(ImagesdelatilsMock, "all")).toEqual(ImagesdelatilsMock);
+  })
+
+  it('should filter one', () => {
+    const pipe = new FilterimagesPipe();
+    const detailExpected = [    
+      { "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" }
+    ]
+    expect(pipe.transform(ImagesdelatilsMock, "perro")).toEqual(detailExpected);
+  })
+
+  it('should filter none', () => {
+    const pipe = new FilterimagesPipe();
+    expect(pipe.transform(ImagesdelatilsMock, "hamster")).toEqual([]);
+  })
 });
+
+const ImagesdelatilsMock = [    
+  { "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
+  { "id": 2, "brand": "gato", "url": "assets/images/perro2.jpg" },
+]
