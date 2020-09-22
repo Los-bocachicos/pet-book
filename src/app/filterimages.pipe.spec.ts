4c10ba6986cf8a7be8ec551aaae968ecc862b76e
Elias Avilez <eliasramos159@gmail.com>
2020-09-22 11:50:41 -0500

    Fix some unit test

--- src/app/filterimages.pipe.spec.ts
+++ src/app/filterimages.pipe.spec.ts
@@ -1,31 +1,31 @@
 import { FilterimagesPipe } from './filterimages.pipe';
 
 describe('FilterimagesPipe', () => {
-  it('create an instance', () => {
+  it('Debe crear la instacia', () => {
     const pipe = new FilterimagesPipe();
     expect(pipe).toBeTruthy();
   });
 
-  it('should filter all', () => {
+  it('Al enviar "all" debe filtrar todas las imagenes', () => {
     const pipe = new FilterimagesPipe();
-    expect(pipe.transform(ImagesdelatilsMock, "all")).toEqual(ImagesdelatilsMock);
-  })
+    expect(pipe.transform(ImagesdelatilsMock, 'all')).toEqual(ImagesdelatilsMock);
+  });
 
-  it('should filter one', () => {
+  it('Al enviar "perro" (que está dentro de la lista) debe filtrar ese elemento', () => {
     const pipe = new FilterimagesPipe();
-    const detailExpected = [    
-      { "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" }
-    ]
-    expect(pipe.transform(ImagesdelatilsMock, "perro")).toEqual(detailExpected);
-  })
+    const detailExpected = [
+      { id: 1, brand: 'perro', url: 'assets/images/perro1.jpg' },
+    ];
+    expect(pipe.transform(ImagesdelatilsMock, 'perro')).toEqual(detailExpected);
+  });
 
-  it('should filter none', () => {
+  it('Al enviar "hamster" (que NO está dentro de la lista) debe retornar una lista vacía', () => {
     const pipe = new FilterimagesPipe();
-    expect(pipe.transform(ImagesdelatilsMock, "hamster")).toEqual([]);
-  })
+    expect(pipe.transform(ImagesdelatilsMock, 'hamster')).toEqual([]);
+  });
 });
 
-const ImagesdelatilsMock = [    
-  { "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
-  { "id": 2, "brand": "gato", "url": "assets/images/perro2.jpg" },
-]
+const ImagesdelatilsMock = [
+  { id: 1, brand: 'perro', url: 'assets/images/perro1.jpg' },
+  { id: 2, brand: 'gato', url: 'assets/images/perro2.jpg' },
+];
--- src/app/image-details/image-details.component.spec.ts
+++ src/app/image-details/image-details.component.spec.ts
@@ -7,32 +7,34 @@ import { ActivatedRoute } from '@angular/router';
 describe('ImageDetailsComponent', () => {
   let component: ImageDetailComponent;
   let fixture: ComponentFixture<ImageDetailComponent>;
-  let mockService : ImageService = new ImageService();
+  let mockService: ImageService = new ImageService();
   let spy: any;
   let spy2: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImageDetailComponent ],
-      providers:[
+      declarations: [ImageDetailComponent],
+      providers: [
         {
           provide: ActivatedRoute,
-          useValue:{
-            snapshot:{
-              params:()=> 2
+          useValue: {
+            snapshot: {
+              params: () => 2,
             },
           },
         },
-        {provide: ImageService , useValue: mockService}
+        { provide: ImageService, useValue: mockService },
       ],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-
- spy = spyOn(mockService,'getImage').and.returnValue({ "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg"});
-   // spy2 =  spyOn(mockService,'getImage').and.returnValue( null);
+    spy = spyOn(mockService, 'getImage').and.returnValue({
+      id: 2,
+      brand: 'perro',
+      url: 'assets/images/perro2.jpg',
+    });
+    // spy2 =  spyOn(mockService,'getImage').and.returnValue( null);
     fixture = TestBed.createComponent(ImageDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -42,26 +44,23 @@ describe('ImageDetailsComponent', () => {
     expect(component).toBeTruthy();
   });
 
- 
-  it("verifica si el metodo de ImageService, getImage fue llamado ", function() {
-    expect(mockService.getImage).toHaveBeenCalled;
+  it('verifica si el metodo de ImageService, getImage fue llamado ', function () {
+    expect(mockService.getImage).toHaveBeenCalled();
   });
 
-  
-
-  it("verifica si el ImageDetail existe y esta definido", function() {
+  it('verifica si el ImageDetail existe y esta definido', function () {
     expect(component.image).toBeDefined();
   });
 
- 
-  it('cuando la imagen existe es 2  debe retornar el json  del imageService indicado',()=>{
-    expect(component.image).toEqual({ "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg"})
-  
+  it('cuando la imagen existe es 2  debe retornar el json  del imageService indicado', () => {
+    expect(component.image).toEqual({
+      id: 2,
+      brand: 'perro',
+      url: 'assets/images/perro2.jpg',
+    });
   });
 
-  
-
-  it('cuando se carga la pantalla , debe existir un contenedor con la clase img-container', ()=>{
+  it('cuando se carga la pantalla , debe existir un contenedor con la clase img-container', () => {
     expect(fixture.nativeElement.querySelector('.img-contaier')).toBeDefined();
-  })
+  });
 });
