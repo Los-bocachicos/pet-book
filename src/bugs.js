0f4bc73574b9b178eaf503a10c55db845670c74f
Mateo Arboleda <marteomasoyyo@gmail.com>
2020-10-21 17:40:52 -0500

    Add bug for demo

--- src/bugs.js
+++ src/bugs.js
@@ -1,26 +1,26 @@
-// var x  = 1;
-// delete x;       // Noncompliant
+var x  = 1;
+delete x;       // Noncompliant
 
-// function foo(){
-// let y = 1
-// }
+function foo(){
+let y = 1
+}
 
-// delete foo;
+delete foo;
 
-// var x = "Foo";
-// "length" in x; // Noncompliant: TypeError
-// 0 in x;        // Noncompliant: TypeError
+var x = "Foo";
+"length" in x; // Noncompliant: TypeError
+0 in x;        // Noncompliant: TypeError
 
-// var a = NaN;
+var a = NaN;
 
-// if (a === NaN) {  // Noncompliant; always false
-//   console.log("a is not a number");  // this is dead code
-// }
-// if (a !== NaN) { // Noncompliant; always true
-//   console.log("a is not NaN"); // this statement is not necessarily true
-// }
+if (a === NaN) {  // Noncompliant; always false
+  console.log("a is not a number");  // this is dead code
+}
+if (a !== NaN) { // Noncompliant; always true
+  console.log("a is not NaN"); // this statement is not necessarily true
+}
 
-// var j = 0;
-// while (true) { // Noncompliant; constant end condition
-//   j++;
-// }
+var j = 0;
+while (true) { // Noncompliant; constant end condition
+  j++;
+}
